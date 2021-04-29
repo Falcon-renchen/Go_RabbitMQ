0f73ae099c580e4eaac3850ce2f2dff0d17ea612
PSH-Falcon <2436482880@qq.com>
2021-04-29 12:45:45 +0800

    rabbitmq with go

---
+++ rabbitmq11_demo/Dlx/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"Go_RabbitMQ/rabbitmq11_demo/Lib"
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func TestDlx(msgs <-chan amqp.Delivery, c string) {
+	for msg := range msgs {
+		fmt.Println("收到消息", string(msg.Body))
+		msg.Ack(false)
+	}
+}
+func main() {
+	client := Lib.NewMQ()
+	err := Lib.DlXTestInit()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Channel.Qos(2, 0, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.Consume("test_dlx", "c", TestDlx)
+
+	defer client.Channel.Close()
+}
--- rabbitmq11_demo/Lib/QueueInit.go
+++ rabbitmq11_demo/Lib/QueueInit.go
@@ -9,18 +9,19 @@ func UserInit() error {
 		return fmt.Errorf("mq init error")
 	}
 	defer mq.Channel.Close()
-
 	//申明交换机
 	err := mq.Channel.ExchangeDeclare(EXCHANGE_USER, "direct", false, false, false, false, nil)
 	if err != nil {
-		return fmt.Errorf("Exchange error")
+		return fmt.Errorf("Exchange error", err)
 	}
-	qs := fmt.Sprintf("%s,%s", QUEUE_NEWUSER, QUEUE_NEWUSER_UNION)
+	//qs:=fmt.Sprintf("%s,%s",QUEUE_NEWUSER,QUEUE_NEWUSER_UNION)
+	qs := fmt.Sprintf("%s", QUEUE_NEWUSER) //这里改动了下，只绑定一个队列
 	err = mq.DecQueueAndBind(qs, ROUTER_KEY_USERREG, EXCHANGE_USER)
 	if err != nil {
 		return fmt.Errorf("Queue Bind error", err)
 	}
 	return nil
+
 }
 
 func UserDelayInit() error {
@@ -29,18 +30,36 @@ func UserDelayInit() error {
 		return fmt.Errorf("mq init error")
 	}
 	defer mq.Channel.Close()
-
 	//申明交换机
 	err := mq.Channel.ExchangeDeclare(EXCHANGE_USER_DELAY, "x-delayed-message",
 		false, false, false, false,
 		map[string]interface{}{"x-delayed-type": "direct"})
 	if err != nil {
-		return fmt.Errorf("Exchange error")
+		return fmt.Errorf("DelayExchange error", err)
 	}
-	qs := fmt.Sprintf("%s,%s", QUEUE_NEWUSER, QUEUE_NEWUSER_UNION)
-	err = mq.DecQueueAndBind(qs, ROUTER_KEY_USERREG, EXCHANGE_USER)
+	qs := fmt.Sprintf("%s", QUEUE_NEWUSER)
+	err = mq.DecQueueAndBind(qs, ROUTER_KEY_USERREG, EXCHANGE_USER_DELAY)
 	if err != nil {
-		return fmt.Errorf("Delay Queue Bind error", err)
+		return fmt.Errorf("Queue Bind error", err)
+	}
+	return nil
+}
+
+//转账相关队列初始化
+func TransInit() error {
+	mq := NewMQ()
+	if mq == nil {
+		return fmt.Errorf("mq init error")
+	}
+	defer mq.Channel.Close()
+	//申明交换机
+	err := mq.Channel.ExchangeDeclare(EXCHANGE_TRANS, "direct", false, false, false, false, nil)
+	if err != nil {
+		return fmt.Errorf("Exchange_Trans error", err)
+	}
+	err = mq.DecQueueAndBind(QUEUE_TRANS, ROUTER_KEY_TRANS, EXCHANGE_TRANS)
+	if err != nil {
+		return fmt.Errorf("Queue Bind error", err)
 	}
 	return nil
 }
